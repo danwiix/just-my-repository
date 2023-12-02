804fef4e660f1e86365d9798569d2783368c7bc8
Daniil <151982171+danwiix@users.noreply.github.com>
2023-12-02 09:12:29 +0500

    0.1
    just new tasks

---
+++ 02.12.23_task3&2.cs
@@ -0,0 +1,10 @@
+﻿int i = 1; // Заменить на 0
+int nums = 0;
+while (i > 0) // Заменить на i < 101
+{
+    i = Convert.ToInt32(Console.ReadLine());
+    nums = nums + 1;
+}
+if (i <= 0) // Заменить на i >= 101
+    Console.WriteLine(nums);
+
