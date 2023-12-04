884883ed9f7902a18ed899bc2952ccb7e7fa6d2d
Daniil <151982171+danwiix@users.noreply.github.com>
2023-12-04 08:48:58 +0500

    0.5

---
+++ 04.12_task7.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num_length = 0;
+
+while(num > 0)
+{
+    num_length++;
+    num = (int)(num / 10);
+}
+Console.WriteLine(num_length);
