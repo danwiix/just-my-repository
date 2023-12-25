5938d73503db8b7e09fadc57102fe927021ed5ab
Daniil <151982171+danwiix@users.noreply.github.com>
2023-12-25 09:35:19 +0500

    idk

---
+++ 25.12_task2.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:");
+
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+
+double b = Convert.ToDouble(Console.ReadLine());
+
+double ab_sum = a + b;
+
+double ab = a * b;
+
+double a_b = a / b;
+
+double a_b2 = a_b % ;
+
+Console.WriteLine($"Сумма:{ab_sum}" + $"\nПроизведение:{ab}" + $"\nЧастноe:{a_b}" 
+    + $"\nОстаток:{a_b2}");
