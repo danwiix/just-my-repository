01a34001ec243839e34c733d7bbda78aee18b63a
Daniil <151982171+danwiix@users.noreply.github.com>
2023-12-25 09:42:27 +0500

    idk

---
+++ 25.12_task3.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваш возраст");
+int age = Convert.ToInt32(Console.ReadLine());
+
+
+if (age >= 18)
+    Console.WriteLine("Вы совершеннолетний.");
+else
+{
+    Console.WriteLine("Вы несовершеннолетний.");
+}
